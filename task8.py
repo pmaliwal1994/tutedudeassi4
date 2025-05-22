7b4a59ed83ac25e69d906b9f333d4256223f9431
Pushpendra <pmaliwal.1994@gmail.com>
2025-05-22 10:15:10 +0530

    Update task8.py

--- task8.py
+++ task8.py
@@ -1,12 +1,21 @@
 #Task 2: Write and Append Data to a File
-fo=open("output.txt","w")
+a="output.txt"
+fo=open(a,"w")
 txt=input("Enter the text for write: ")
 res=txt +"\n"
 fo.write(res)
 fo.close()
 
-fo=open("output.txt","a")
+fo=open(a,"a")
 txt=input("Enter the text for append: ")
 res=txt +"\n"
 fo.write(res)
 fo.close()
+
+try:
+    fileread=open(a,'r')
+    f=fileread.readlines()
+    for i in f:
+        print(i,end=" ")
+except :
+    print(f'{a} File not found')
